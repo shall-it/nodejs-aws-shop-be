b177b6916e644b485bfd093f95d9f4b2c7bef4c1
Aleksei Shulga <ashulga@transperfect.com>
2023-11-26 20:09:36 +0300

    Additional tasks

--- product-service/src/handlers/createProductDdb.ts
+++ product-service/src/handlers/createProductDdb.ts
@@ -4,6 +4,32 @@ const docClient = new DynamoDB.DocumentClient();
 
 exports.handler = async (event: any) => {
     const body = JSON.parse(event.body);
+
+    const fieldsToCheck = [
+        { name: 'id', type: 'string', required: true },
+        { name: 'title', type: 'string', required: true },
+        { name: 'description', type: 'string', required: false },
+        { name: 'price', type: 'number', required: true },
+        { name: 'count', type: 'number', required: true }
+    ];
+
+    for (const field of fieldsToCheck) {
+        const value = body[field.name];
+        if (field.required && !value) {
+            return buildResponse(400,
+                {
+                    message: `Invalid or missing ${field.name}`
+                });
+        }
+        if (value && typeof value !== field.type) {
+            return buildResponse(400,
+                {
+                    message: `Invalid type of ${field.name}`
+                });
+        }
+    }
+
+
     const table1Name = process.env.TABLE1_NAME;
     const table2Name = process.env.TABLE2_NAME;
 
@@ -11,30 +37,34 @@ exports.handler = async (event: any) => {
         throw new Error('Environment variables TABLE1_NAME and TABLE2_NAME must be set');
     }
 
-    const params1 = {
-        TableName: table1Name,
-        Item: {
-            id: body.id,
-            description: body.description,
-            price: body.price,
-            title: body.title
-        }
-    };
-
-    const params2 = {
-        TableName: table2Name,
-        Item: {
-            product_id: body.id,
-            count: body.count
-        }
+    const params = {
+        TransactItems: [
+            {
+                Put: {
+                    TableName: table1Name,
+                    Item: {
+                        id: body.id,
+                        description: body.description,
+                        price: body.price,
+                        title: body.title
+                    }
+                }
+            },
+            {
+                Put: {
+                    TableName: table2Name,
+                    Item: {
+                        product_id: body.id,
+                        count: body.count
+                    }
+                }
+            }
+        ]
     };
 
     try {
-        await docClient.put(params1).promise();
-        console.log('Creation of product in products is succeeded');
-
-        await docClient.put(params2).promise();
-        console.log('Creation of product in stocks is succeeded');
+        await docClient.transactWrite(params).promise();
+        console.log('Creation of product in products and stocks is succeeded');
 
         return buildResponse(200,
             {
